5ab48a5c34d1a51943d8e8a0a2dd4c78391580d9
Jan Lamayo <cachojanbenedict010896@gmail.com>
2023-04-09 08:13:35 +0800

    Operators

---
+++ basic-advance-csharp-console/Exercises/Basic/Operators.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_advance_csharp_console.Exercises.Basic
+{
+    public class Operators
+    {
+
+        double _a, _b, _c;
+
+
+        public Operators(double a, double b, double c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+
+        public void ViewResult()
+        {
+            Console.WriteLine("THE VALUE OF FIRST NUMBER IS: " + _a + " AND THE VALUE OF SECOND NUMBER IS: " + _b);
+            Console.WriteLine("THIS IS SAMPLE OF ADDITION LOGIC. THE RESULT OF FIRST NUMBER(" + _a + ") PLUS SECOND NUMBER(" + _b + ") IS: " + SampleAddition(_a, _b));
+            Console.WriteLine("THIS IS SAMPLE OF SUBTRACTION LOGIC. THE RESULT OF FIRST NUMBER(" + _a + ") MINUS SECOND NUMBER(" + _b + ") IS: " + SampleSubtraction(_a, _b));
+            Console.WriteLine("THIS IS SAMPLE OF MULTIPLICATION LOGIC. THE RESULT OF FIRST NUMBER(" + _a + ") TIMES SECOND NUMBER(" + _b + ") IS: " + SampleMultiplication(_a, _b));
+            Console.WriteLine("THIS IS SAMPLE OF DIVIDE LOGIC. THE RESULT OF FIRST NUMBER(" + _a + ") DIVIDE SECOND NUMBER(" + _b + ") IS: " + SampleDivide(_a, _b));
+           
+        }
+
+        public double SamplePublicDivide(double a, double c)
+        {
+            return a / c; 
+        }
+
+       
+        private double SampleAddition(double a, double b)
+        {
+            return a + b;
+        }
+
+        private double SampleSubtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        private double SampleMultiplication(double a, double b)
+        {
+            return a * b; 
+        }
+
+        private double SampleDivide(double a, double b)
+        {
+
+            return a / b;
+
+
+        }
+
+    }
+}
--- basic-advance-csharp-console/Program.cs
+++ basic-advance-csharp-console/Program.cs
@@ -14,8 +14,25 @@ namespace BasicToAdvanceExercises
 
 
             //1st exercise:
-            var basicSyntax = new BasicSyntax();
-            basicSyntax.ViewResult();
+            //var basicSyntax = new BasicSyntax();
+            //basicSyntax.ViewResult();
+
+
+            //2nd Exercise:Operators with encapsulation
+           // Console.WriteLine("First Number:");
+           // double a = Convert.ToDouble(Console.ReadLine());
+           // Console.WriteLine("Second Number:");
+           // double b = Convert.ToDouble(Console.ReadLine());
+           // Console.WriteLine("Third Number:");
+           // double c = Convert.ToDouble(Console.ReadLine());
+
+           // // Method logic is hidden.
+           // var operators = new Operators(a,b,c);
+           // operators.ViewResult();
+
+           ////Show in public what kind of method logic will run.
+           // Console.WriteLine("THIS IS SAMPLE OF PUBLIC DIVIDE LOGIC. THE RESULT OF FIRST NUMBER(" + a + ") DIVIDE THIRD NUMBER(" + c + ") IS: " + operators.SamplePublicDivide(a, c));
+
         }
     }
 }
